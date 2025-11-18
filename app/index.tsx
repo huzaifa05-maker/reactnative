15eadedc9668f93c20910165e7ec94945e5ff4c9
huzaifa05-maker <hmhuzaifakhan540@gmail.com>
2025-11-18 11:27:12 +0500

    add globalstyles file

--- app/index.tsx
+++ app/index.tsx
@@ -1,29 +1,14 @@
-import { Link } from 'expo-router';  // ✅ Add this line
-import { View, Text } from 'react-native';
-import SuccessButton from '@/components/SuccessButton';
+
+import { globalStyles } from '@/styles/globalstyles';
+import { View, Text, StyleSheet } from 'react-native';
+
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>This is home page</Text>
-      <Link href="/about">
-        <Text>Go to About</Text>
-      </Link>
-      <Link href="/product/index">
-        <Text>Go to Product</Text>
-      </Link>
-      <Link href="/blog/[blogid]">
-        <Text>Go to Blog</Text>
-      </Link>
-
-      <SuccessButton title="Testing"/>
+    <View style={globalStyles.container} >    
+      <Text style={globalStyles.heading}>Hello World !!!</Text>
 
     </View>
   );
 }
+
---
+++ styles/globalstyles.ts
@@ -0,0 +1,16 @@
+import { StyleSheet } from "react-native";
+
+export const globalStyles = StyleSheet.create({  
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#18a2deff',
+  },
+  heading:{
+    fontSize:24,
+    fontWeight:'bold',
+    marginBottom:20,
+    color:'yellow',
+  }
+})
